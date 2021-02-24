79f240827070fcb3c5705a241c76869ceb4a97e5
meleciosilva <mpsilva94@gmail.com>
2021-02-24 09:05:39 -0500

    refactored functions

--- script.js
+++ script.js
@@ -1,68 +1,61 @@
 const breweries = document.querySelector('.breweries');
+const breweryListings = document.querySelector('.breweries').children;
 
 // retrieves brewery data and renders all breweries on webpage
-const listAllBreweries = async () => {
-  const results = await axios.get("https://api.openbrewerydb.org/breweries?by_state=virginia").then(({data}) => data);
+async function renderAllBreweries() {
+  const results = await axios.get("https://api.openbrewerydb.org/breweries?by_state=virginia");  
+  results.data.forEach(brewery => {
+    renderOneBrewery(brewery);
+  })
+}
 
-  for (let i = 0; i < results.length; i++) {
-    const content = `
-    <div class='col-6 col-md-3 py-2'>
-      <div class="card" style='height:100%'>
-        <h5 class="card-header">${results[i].name}</h5>
-        <div class="card-body d-flex flex-column justify-content-between">
-          <h6>Address:</h6>
-          <p>${results[i].street} ${results[i].city}, ${results[i].state}</p>
-          <h6>Type: ${results[i].brewery_type.charAt(0).toUpperCase() + results[i].brewery_type.slice(1)}</h6>
-          <a href="${results[i].website_url}" target="_blank" class="btn btn-success">Visit Website</a>
-        </div>
+//renders one brewery listing
+function renderOneBrewery(listing) {
+  const content = `
+  <div class='col-6 col-md-3 py-2'>
+    <div class="card" style='height:100%'>
+      <h5 class="card-header">${listing.name}</h5>
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h6>Address:</h6>
+        <p>${listing.street} ${listing.city}, ${listing.state}</p>
+        <h6 id='type'>Type: ${listing.brewery_type.charAt(0).toUpperCase() + listing.brewery_type.slice(1)}</h6>
+        <a href="${listing.website_url}" target="_blank" class="btn btn-success">Visit Website</a>
       </div>
     </div>
-    `;
-    breweries.insertAdjacentHTML('beforeend', content);
-  }
+  </div>
+  `;
+  breweries.insertAdjacentHTML('beforeend', content);
 }
 
 // renders brewery listings based on search passed
-async function searchByName(search) {
-  const results = await axios.get("https://api.openbrewerydb.org/breweries?by_state=virginia").then(({data}) => data);
-
-  const list = [];
+function searchByName(search) {
   const searchLower = search.toLowerCase();
-  for (let i = 0; i < results.length; i++) {
-    const name = results[i].name.toLowerCase();
-    const type = results[i].brewery_type.toLowerCase();
-    const city = results[i].city.toLowerCase();
-    if ( name.includes(searchLower) || type.includes(searchLower) || city.includes(searchLower) ) {
-      const content = `
-        <div class='col-6 col-md-3 py-2'>
-          <div class="card" style='height:100%'>
-            <h5 class="card-header">${results[i].name}</h5>
-            <div class="card-body d-flex flex-column justify-content-between">
-              <h6>Address:</h6>
-              <p>${results[i].street} ${results[i].city}, ${results[i].state}</p>
-              <h6>Type: ${results[i].brewery_type.charAt(0).toUpperCase() + results[i].brewery_type.slice(1)}</h6>
-              <a href="${results[i].website_url}" target="_blank" class="btn btn-success">Visit Website</a>
-            </div>
-          </div>
-        </div>
-      `;
-      list.push(content);
+  for (let brewery of breweryListings) {
+    const breweryName = brewery.querySelector('.card-header').textContent.toLowerCase();
+    const breweryAddress = brewery.querySelector('.card-body p').textContent.toLowerCase();
+    const breweryType = brewery.querySelector('.card-body #type').textContent.toLowerCase();
+    if ( !breweryName.includes(searchLower) && !breweryAddress.includes(searchLower) && !breweryType.includes(searchLower) ) {
+      brewery.classList.add("hidden");
+    } else {
+      brewery.classList.remove("hidden");
     }
   }
-  breweries.innerHTML = list.join('');
 }
 
-// selects search bar
-const searchInput = document.querySelector('input');
+function filterSearch() {
+  // selects search bar
+  const searchInput = document.querySelector('input');
 
-// filters list of breweries by search input
-searchInput.addEventListener('keyup', (event) => {
-  const input = searchInput.value;
-  searchByName(input);
-})
+  // filters list of breweries by search input
+  searchInput.addEventListener('keyup', (event) => {
+    const input = searchInput.value;
+    searchByName(input);
+  })
+}
 
 function main() {
-  listAllBreweries();
+  renderAllBreweries();
+  filterSearch();
 }
 
 window.addEventListener("DOMContentLoaded", main);
