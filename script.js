f8a5a4ea4bd913720c8c27682dc2ab0683cc09ee
meleciosilva <mpsilva94@gmail.com>
2021-03-09 21:49:00 -0500

    added pagination buttons

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const breweries = document.querySelector('.breweries');
 const breweryListings = document.querySelector('.breweries').children;
+const paginationDiv = document.querySelector(".pagination-buttons");
+const perPage = 8; // number of breweries to display per page
 
-// retrieves brewery data and renders all breweries on webpage
-async function renderAllBreweries() {
-  const results = await axios.get("https://api.openbrewerydb.org/breweries?by_state=virginia");  
-  results.data.forEach(brewery => {
-    renderOneBrewery(brewery);
+
+async function allBreweries() {
+  const results = await axios.get("https://api.openbrewerydb.org/breweries?by_state=virginia").then(({data}) => data);
+  return results;
+}
+
+// renders list of breweries by selected page number
+function renderBreweries(list, page) {
+  const startIndex = (page * perPage) - perPage;
+  const endIndex = page * perPage - 1;
+  list.forEach((brewery, index) => {
+    if (index >= startIndex && index <= endIndex) {
+      renderOneBrewery(brewery);
+    }
   })
 }
 
@@ -27,35 +38,73 @@ function renderOneBrewery(listing) {
   breweries.insertAdjacentHTML('beforeend', content);
 }
 
-// renders brewery listings based on search passed
-function searchByName(search) {
-  const searchLower = search.toLowerCase();
-  for (let brewery of breweryListings) {
-    const breweryName = brewery.querySelector('.card-header').textContent.toLowerCase();
-    const breweryAddress = brewery.querySelector('.card-body p').textContent.toLowerCase();
-    const breweryType = brewery.querySelector('.card-body #type').textContent.toLowerCase();
-    if ( !breweryName.includes(searchLower) && !breweryAddress.includes(searchLower) && !breweryType.includes(searchLower) ) {
-      brewery.classList.add("hidden");
-    } else {
-      brewery.classList.remove("hidden");
-    }
+// adds pagination buttons
+function addPagination(list) {
+  let totalButtons = Math.ceil(list.length / perPage); 
+  paginationDiv.innerHTML = '';
+  
+  // displays total number of buttons with correct page number
+  for (let i = 0; i < totalButtons; i++) {
+     let button = `<button type="button" class="btn btn-secondary mx-1">${i + 1}</button>`;
+     paginationDiv.insertAdjacentHTML("beforeend", button);
+  }
+  
+  // assigns first button active class to indicate page 1 selected
+  const firstButton = paginationDiv.firstElementChild;
+  firstButton.classList.add('active-button');
+
+  // assigns event listener for pagination buttons - reloads page with appropriate listings
+  const paginationButtons = document.querySelectorAll("button");
+  for (let button of paginationButtons) {
+    button.addEventListener("click", () => {
+      // removes selected class from previously active button
+      const activeButton = document.querySelector('.active-button');
+      activeButton.classList.remove('active-button');
+      button.classList.add('active-button');
+      // re-renders page with appropriate breweries
+      breweries.innerHTML = '';
+      // renderBreweriesByPage(button.textContent);
+      renderBreweries(list, button.textContent)
+    })
   }
 }
 
-function filterSearch() {
-  // selects search bar
-  const searchInput = document.querySelector('input');
+// renders brewery listings based on search passed
+async function searchEntry(search) {
+  const searchLower = search.toLowerCase();
 
+  // filters brewery listings based on search input and re-renders matches
+  const filteredBreweries = (await allBreweries()).filter(brewery => {
+    if (
+      brewery.name.toLowerCase().includes(searchLower) || 
+      brewery.street.toLowerCase().includes(searchLower) || 
+      brewery.city.toLowerCase().includes(searchLower) || 
+      brewery.brewery_type.toLowerCase().includes(searchLower)
+    ) return brewery;
+  })
+  // removes listings and re-renders matches
+  breweries.innerHTML = "";
+  renderBreweries(filteredBreweries, 1);
+  // adds pagination buttons
+  addPagination(filteredBreweries);
+}
+
+// selects search field and invokes searchEntry function with each change
+function filterSearch() {
+  const searchInput = document.querySelector('input');
   // filters list of breweries by search input
-  searchInput.addEventListener('keyup', (event) => {
+  searchInput.addEventListener('keyup', () => {
     const input = searchInput.value;
-    searchByName(input);
+    searchEntry(input);
   })
 }
 
-function main() {
-  renderAllBreweries();
+// renders all breweries, pagination buttons, and activates filterSearch function/event listener
+async function main() {
+  renderBreweries((await allBreweries()), 1);
+  addPagination(await allBreweries());
   filterSearch();
 }
 
+// invokes main function upon DOM content loading
 window.addEventListener("DOMContentLoaded", main);
